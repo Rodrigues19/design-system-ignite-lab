5b6bc95fca246533d3a463d153d865baedb5b856
Larissa Rodrigues <rodrigueslarissacr@gmail.com>
2022-10-18 10:33:46 -0300

    commit test

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -15,7 +15,7 @@ export function Signin(){
                 <Logo/>
 
                 <Heading size='lg' className="mt-4">
-                    Ignite Lab
+                    Ignite Labtr
                 </Heading>
 
                 <Text size="lg" className="text-gray-400 mt-1">Faça login e comece a usar!</Text>
